276e639da3e67ff546e68d315b87c7913c3a72c6
terehov-space <terehov.space@gmail.com>
2020-11-13 02:58:45 +0300

    Adding link to adding new file

--- src/resources/views/rows.blade.php
+++ src/resources/views/rows.blade.php
@@ -28,6 +28,7 @@
         </div>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <a href="/" class="btn btn-primary">Add to parse</a>
                 <div id="accordion">
                     @foreach ($rows as $rowDate => $row)
                     <div class="card">
